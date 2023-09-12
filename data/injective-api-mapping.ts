158f7ff451d9715adc7b195967128bde8f35a5df
smealio <100466569+smealio@users.noreply.github.com>
2023-09-11 20:54:45 -0700

    added datafile

---
+++ data/injective-api-mapping.ts
@@ -0,0 +1,15 @@
+type InjectiveApiQueryMapping = {
+    name: string;
+    base64value:string;
+}
+
+type InjectiveApiMapping = {
+    name: string;
+    uri: string;
+}
+
+export const InjectiveApiMappings: InjectiveApiQueryMapping[] = [
+    {
+        name:"get_"
+    }
+]
