a99eb60267d3c1469ed1bb24ec50177591b9dec9
Morgan Metz <mlmi4@k12albemarle.org>
2023-09-21 11:33:22 -0400

    Create ContractTypes.ts

---
+++ utils/ContractTypes.ts
@@ -0,0 +1,56 @@
+export interface MintMsg {
+    // This is an empty message, the contract does most of the heavy lifting.
+}
+
+export interface BuyMsg { // Execute msg needs the funds to be set
+    token_id: string;
+}
+
+export interface ListMsg {
+    token_id: string;
+    price: string;
+    expires: bigint; // If no expirey set, make it the same date the sun is supposed to explode 👍
+}
+
+export interface RoyaltyInfo {
+    share: number;
+    address: String;
+}
+
+export interface ClaimCollectionMsg {
+    banner_uri: null | String;
+    avatar_uri: null | String;
+    description: null | String;
+    basis_points: null | number; // 100 == 1% royalty
+    creators: null | Array<RoyaltyInfo>
+}
+
+export interface ExecuteWrapperMsg {
+    Mint?: MintMsg;
+    Buy?: BuyMsg;
+    List?: ListMsg;
+    UpdateMetadata?: ClaimCollectionMsg;
+}
+
+export interface QueryWrapperMsg {
+    GetMetadata?: {};
+    GetListed?: {};
+}
+/*
+Fully constructed messages look like this when serialized:
+{
+    "sender": "",
+    "contract": "",
+    "msg": {
+        "action": { // "action" must be replaced with the message name. i.e, mint, buy, sell, list, etc.
+            "args": ""
+        }
+    },
+    "funds": [
+        {
+            "denom": "inj",
+            "amount": "" // for some reason this is still a string. Rust converts it to Cosmos' Uint128 type.
+        }
+    ]
+}
+*/
