e6c4592fe817fca3c36b83b00a9b2e2f5765e8d3
Morgan Metz <mlmi4@k12albemarle.org>
2023-10-05 10:43:32 -0400

    Update signMessage.ts

--- utils/signMessage.ts
+++ utils/signMessage.ts
@@ -1,12 +1,12 @@
 import { MsgInstantiateContract, MsgExecuteContract, useShuttle } from "@delphi-labs/shuttle-react";
 import useWallet from "@/hooks/useWallet"; 
 
-export function signWasmMsg(msg: MsgExecuteContract | MsgInstantiateContract) {
+export function signWasmMsg(msg: MsgExecuteContract[] | MsgInstantiateContract[]) {
     const wallet = useWallet();
     const { sign } = useShuttle();
 
     return sign({
-        messages: [msg],
+        messages: msg,
         feeAmount: "1000000000", 
         gasLimit: "3000000000", 
         memo: "",
