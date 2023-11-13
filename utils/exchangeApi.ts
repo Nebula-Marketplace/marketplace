3ef137fceb7aaee110c1dc6f161491b0a8867035
Morgan Metz <mlmi4@k12albemarle.org>
2023-11-13 13:24:47 -0500

    blank

--- utils/exchangeApi.ts
+++ utils/exchangeApi.ts
@@ -10,7 +10,7 @@ import axios from 'axios';
 const codeID =169;
 const talis_nft = 49;
  const nebula_nft = 200; // TODO: update this wen nebula standard is out
-const network = "mainnet"== "mainnet" ? Network.Mainnet : Network.Mainnet; 
+const network = "mainnet" == "mainnet" ? Network.Mainnet : Network.Mainnet; 
 console.log(process.env.NETWORK)
 interface GetTokensResponse { 
     ids: string[]
