45b75501f99ee1bca9094ae8ca2b141b7009ebbc
Morgan Metz <mlmi4@k12albemarle.org>
2023-11-25 21:44:51 -0500

    Update collections.ts

--- data/mock/collections.ts
+++ data/mock/collections.ts
@@ -2,39 +2,35 @@ import { Collection } from "../types/Collection";
 
 export const collections: Collection[] = [
     {
-        Name:"A5tound",
-        ContractAddress: "inj10dwdq2rgp2fcxpsecfc0u7jruurexgwun2tu3s",
-        Symbol:"A5T",
-        Supply: 200,
-        Metadata: {
-            Banner:"/assets/images/collections/a5tound/banner.jpeg",
-            Cover: "/assets/images/collections/a5tound/cover.jpeg",
-            Description:"For the community. Ran by the community. Brand , Inspired by Fashion , Music , and the 90s",
-            Logo: "",
-            Discord:"",
-            Twitter:"",
-            Telegram:"",
-            Email:"",
-            Website:"",
-            Atlas3: ""
-        }
+        collection:"A5tound",
+        contract: "inj10dwdq2rgp2fcxpsecfc0u7jruurexgwun2tu3s",
+        symbol:"A5T",
+        supply: 200,
+        banner_uri:"/assets/images/collections/a5tound/banner.jpeg",
+        logo_uri: "/assets/images/collections/a5tound/cover.jpeg",
+        description:"For the community. Ran by the community. Brand , Inspired by Fashion , Music , and the 90s",
+        discord:"",
+        twitter:"",
+        telegram:"",
+        website:"",
+        creators:[],
+        basis_points: 0,
+        owner: ""
     },
     {
-        Name:"Hobos",
-        ContractAddress: "inj1uppxsg2c2e9y6xptx0kzxkwllq54vthtzxd8r4",
-        Symbol:"HOBO",
-        Supply: 5000,
-        Metadata: {
-            Banner:"/assets/images/collections/hobos/banner.jpeg",
-            Cover: "/assets/images/collections/hobos/cover.jpeg",
-            Description:"For the community. Ran by the community. Brand , Inspired by Fashion , Music , and the 90s",
-            Logo: "",
-            Discord:"",
-            Twitter:"",
-            Telegram:"",
-            Email:"",
-            Website:"",
-            Atlas3: ""
-        }
+        collection:"Hobos",
+        contract: "inj1uppxsg2c2e9y6xptx0kzxkwllq54vthtzxd8r4",
+        symbol:"HOBO",
+        supply: 5000,
+        banner_uri:"/assets/images/collections/hobos/banner.jpeg",
+        logo_uri: "/assets/images/collections/hobos/cover.jpeg",
+        description:"For the community. Ran by the community. Brand , Inspired by Fashion , Music , and the 90s",
+        discord:"",
+        twitter:"",
+        telegram:"",
+        website:"",
+        creators:[],
+        basis_points: 0,
+        owner: ""
     },
 ]
