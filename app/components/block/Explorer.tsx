5622285c5d16d21d9c2284fdcb1141fe81c353d0
Morgan Metz <mlmi4@k12albemarle.org>
2023-12-12 13:40:02 -0500

    Update Explorer.tsx

--- app/components/block/Explorer.tsx
+++ app/components/block/Explorer.tsx
@@ -4,11 +4,30 @@ import ProductCard from "../card/ProductCard";
 import Link from "next/link";
 import CollectionCard from "../card/CollectionCard";
 import { useGlobalState } from "@/utils/GlobalContext";
+import { useEffect, useState } from "react";
+import { fetchNftContractState } from "@/utils/exchangeApi";
 
 export default function Explore4({exchanges,listedNfts}:any) {
     console.log(exchanges)
     const { listed, setListed, collections, setCollections } = useGlobalState();
+    const [ filtered, setFiltered ] = useState<string[]>([]);
     console.log(collections)
+
+    useEffect(() => {
+        const checkFlags = async () => {
+            const promises = exchanges.map(async (address: string) => {
+                const state = await fetchNftContractState(address);
+                return { address, flagged: state.flagged };
+            });
+
+            const results = await Promise.all(promises);
+            const flaggedAddresses = results.filter(item => !item.flagged).map(item => item.address);
+            setFiltered(flaggedAddresses);
+        };
+
+        checkFlags();
+    }, [exchanges]);
+
     return (
         <>
             <section className="tf-explore tf-section">
@@ -19,11 +38,11 @@ export default function Explore4({exchanges,listedNfts}:any) {
                         </div> */}
                         <div className="col-xl-9 col-lg-9 col-md-12">
                             <div className="box-epxlore">
-                                {collections.map((item) => {
+                                {filtered.map((item: string, index: any) => {
                                 console.log(item)
                                 return(
                                     <div
-                                        key={item.id}
+                                        key={index}
                                         className="col-xl-4 col-lg-4 col-md-6 col-sm-6 justify-content-around"
                                     >
                                         <CollectionCard data={item} />
